50b147b11329da00140b34afe514b0c4aad8d551
samodra <eka.samod@gmail.com>
2015-04-07 00:56:06 +0700

    modul8

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author eka
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Saya eka. Nim Saya 6306130079. Saya sangat menyukai praktikum RPL Modul 8 ini");
+    }
+}
